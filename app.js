64119b2b172c9a2ac7597ee6d793d0d0d6fe7739
aworsley9@googlemail.com <1309Adam1309Adam>
2020-07-01 21:44:03 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js')
+const config = require('./config.json')
+const request = require('request')
+const client = new Discord.Client()
+
+function redeemcode(code) {
+    var options = {
+    'method': 'POST',
+    'url': 'https://ptb.discordapp.com/api/v6/entitlements/gift-codes/' + code + '/redeem',
+    'headers': {
+        'Authorization': config.token,
+    }
+    };
+    request(options, function (error, response) { 
+    if (error) throw new Error(error);
+       console.log(response.body + '\n');
+    });
+}
+
+client.on('ready', () => {
+    console.log("Sniping as " + client.user.tag)
+})
+
+client.on('message', message => {
+
+    contentsplit = message.content.split(' ')
+    var code = ''
+    var x
+    for(x in contentsplit) {
+        if(contentsplit[x].startsWith('https://discord.gift/')) {
+            code = contentsplit[x].replace('https://discord.gift/', '')
+            console.log('\nServer: ' + message.guild.name + '\n' + 'Author: ' + message.author.username + '\n' + 'Channel: ' + message.channel.name + '\n' + 'Gift code: ' + code)
+            redeemcode(code)
+        }
+        else if(contentsplit[x].startsWith('discord.gift/')) {
+            code = contentsplit[x].replace('discord.gift/', '')
+            console.log('\nServer: ' + message.guild.name + '\n' + 'Author: ' + message.author.username + '\n' + 'Channel: ' + message.channel.name + '\n' + 'Gift code: ' + code)
+            redeemcode(code)
+        }
+    }
+})
+
+client.login(config.token)
